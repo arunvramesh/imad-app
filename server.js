f0298913f1e5c1f78dc90f2fc90fe6271979e933
arunvramesh <30728698+arunvramesh@users.noreply.github.com>
2017-08-05 15:31:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one.html', function(req,res){
   // res.send("article one"); 
-   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two.html', function(req,res){
