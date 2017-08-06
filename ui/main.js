7e029973a19a77dd494e5abdea814b66f2cd98c0
arunvramesh <30728698+arunvramesh@users.noreply.github.com>
2017-08-06 11:25:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,8 @@
 console.log('Loaded!');
 alert("Java script");
+
+//move left
+var img=document.getElementById("madi");
+img.onClick= function(){
+    element.innerHtml = `<marquee behavior="scroll" direction="left"><img id="madi" src="/ui/madi.png" class="img-medium"/></marquee>>`;
+};
