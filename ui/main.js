c1239345e3a11de10f6c3a21073f184f166a8da6
arunvramesh <30728698+arunvramesh@users.noreply.github.com>
2017-08-06 11:51:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 //alert("Java script");
 
 //move left
-var image=document.getElementById('madi');
+var image=document.getElementById("pic");
 image.onclick = function () {
   //  img.style.marginLeft = "20px";
 };
