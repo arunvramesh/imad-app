12862b5fafc8551f7ffa236b3fb796d8e79b2fba
arunvramesh <30728698+arunvramesh@users.noreply.github.com>
2017-08-13 21:47:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article/:articlename', function(req,res){
+/*app.get('/article/:articlename', function(req,res){
     var articlename= req.params.articlename;
     pool.query("SELECT * from article where title='"+req.params.articlename+"'", function(err,result){
        if(err){
@@ -135,7 +135,7 @@ app.get('/article/:articlename', function(req,res){
            }
        }
     });
-});
+});*/
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
